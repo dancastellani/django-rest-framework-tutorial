95f9af97328a8bdeac3cebae7ff117dfad9d51e4
Daniel Castellani <dancastellani@gmail.com>
2018-02-12 19:57:29 -0500

    Finished tutorial part-7. http://www.django-rest-framework.org/tutorial/7-schemas-and-client-libraries/

---
+++ README.md
@@ -0,0 +1,3 @@
+
+[Tutorial](http://www.django-rest-framework.org/tutorial/1-serialization/)
+[Reference source code](https://github.com/encode/rest-framework-tutorial/tree/master/snippets)
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 from snippets import views
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Django Rest Framework Tutorial')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -9,7 +12,8 @@ router.register(r'users', views.UserViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    url(r'^', include(router.urls))
+    url(r'^schema/$', schema_view),
+    url(r'^', include(router.urls)),
 ]
 
 # Api auth urls
